55e0c186e423597dd6de4f51fda5208ecce456ba
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-07 12:13:24 +0300

    Привязка свойств Статус к тексту в Статус-баре

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -13,5 +13,20 @@ namespace CV19.ViewModels
             set => Set(ref _Title, value);
         }
         #endregion
+
+        #region Status : string - Статус программы 
+
+        /// <summary>Статус программы.</summary>
+        private string _Status =  "Готов!";
+
+        /// <summary>Статус программы.</summary>
+        public string Status 
+        { 
+            get => _Status;
+            set => Set(ref _Status, value); 
+        }
+        #endregion
+
+
     }
 }

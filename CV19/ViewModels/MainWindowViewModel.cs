e9401643b5616548321d716882ad0847ca080ed1
Владимир Лапочкин <vovan11999@yandex.ru>
2025-06-08 23:13:35 +0300

    Добавляем тестовые данные в модель-представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 using System.Windows;
 using System.Windows.Input;
@@ -6,6 +7,17 @@ namespace CV19.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region Тестовый набор данных для визуализации графиков
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        private IEnumerable<DataPoint> _TestDataPoints;
+        /// <summary>Тестовый набор данных для визуализации графиков</summary>
+        public IEnumerable<DataPoint> TestDataPoints
+        { 
+            get => _TestDataPoints; 
+            set => Set(ref _TestDataPoints, value); 
+        }
+        #endregion
+
         #region Заголовок окна
         private string _Title = "Анализ статистики CV19";
 
@@ -51,8 +63,19 @@ namespace CV19.ViewModels
             #region Команды
 
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
-            
+
             #endregion
+            var data_points = new List<DataPoint>((int)(360/0.1));
+            for (var x = 0d; x <= 360; x+=0.1) 
+            {
+                const double TO_RAD = Math.PI / 180;
+                var y = Math.Sin(x * TO_RAD);
+
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+            
+            TestDataPoints = data_points;
+            
         }
     }
 }
